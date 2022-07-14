08278d49f1a40e10fd75dfba51fc3ff839a9c814
Paulo Braga <94999843+PaloBraga@users.noreply.github.com>
2022-07-14 14:28:48 -0300

    Create views.py

---
+++ contas/views.py
@@ -0,0 +1,38 @@
+from mailbox import NotEmptyError
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .form import TransacaoForm
+import datetime
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
+    #html = "<html><body>O dia e hora correto é %s.</body></html>" % now (original)   
+    return render(request,'contas/home.html', data)
+ 
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None )
+    
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.filter(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
